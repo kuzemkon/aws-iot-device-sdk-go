56f6bae34d7e87c94c4516f3b64c0fe878a68773
kuzemkon <yurakuzemko@gmail.com>
2020-02-19 14:23:12 +0200

    extend the device communication functionality

--- device/thing.go
+++ device/thing.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"path"
 	"time"
 
 	"github.com/eclipse/paho.mqtt.golang"
@@ -18,7 +19,7 @@ type Thing struct {
 }
 
 // ThingName the name of the AWS IoT device representation
-type ThingName string
+type ThingName = string
 
 // KeyPair the structure contains the path to the AWS MQTT credentials
 type KeyPair struct {
@@ -29,6 +30,14 @@ type KeyPair struct {
 
 // Shadow device shadow data
 type Shadow []byte
+
+// String converts the Shadow to string
+func (s Shadow) String() string {
+	return string(s)
+}
+
+// ShadowError represents the model for handling the errors occurred during updating the device shadow
+type ShadowError = Shadow
 
 // NewThing returns a new instance of Thing
 func NewThing(keyPair KeyPair, awsEndpoint string, thingName ThingName) (*Thing, error) {
@@ -45,7 +54,7 @@ func NewThing(keyPair KeyPair, awsEndpoint string, thingName ThingName) (*Thing,
 
 	tlsConfig := &tls.Config{
 		Certificates: []tls.Certificate{tlsCert},
-		RootCAs: certs,
+		RootCAs:      certs,
 	}
 
 	if err != nil {
@@ -71,10 +80,21 @@ func NewThing(keyPair KeyPair, awsEndpoint string, thingName ThingName) (*Thing,
 	}, nil
 }
 
-// GetThingShadow gets the current thing shadow
+// Disconnect terminates the MQTT connection between the client and the AWS server. Recommended to use in defer to avoid
+// connection leaks.
+func (t *Thing) Disconnect() {
+	t.client.Disconnect(1)
+}
+
+// GetThingShadow returns the current thing shadow
 func (t *Thing) GetThingShadow() (Shadow, error) {
 	shadowChan := make(chan Shadow)
 	errChan := make(chan error)
+
+	defer t.unsubscribe(
+		fmt.Sprintf("$aws/things/%s/shadow/get/accepted", t.thingName),
+		fmt.Sprintf("$aws/things/%s/shadow/get/rejected", t.thingName),
+	)
 
 	if token := t.client.Subscribe(
 		fmt.Sprintf("$aws/things/%s/shadow/get/accepted", t.thingName),
@@ -121,25 +141,146 @@ func (t *Thing) GetThingShadow() (Shadow, error) {
 	}
 }
 
-// UpdateThingShadow publish a message with new thing shadow
+// UpdateThingShadow publishes an async message with new thing shadow
 func (t *Thing) UpdateThingShadow(payload Shadow) error {
 	token := t.client.Publish(fmt.Sprintf("$aws/things/%s/shadow/update", t.thingName), 0, false, []byte(payload))
 	token.Wait()
 	return token.Error()
 }
 
-// SubscribeForThingShadowChanges returns the channel with the shadow updates
-func (t *Thing) SubscribeForThingShadowChanges() (chan Shadow, error) {
+// SubscribeForThingShadowChanges subscribes for the device shadow update topic and returns two channels: shadow and shadow error.
+// The shadow channel will handle all accepted device shadow updates. The shadow error channel will handle all rejected device
+// shadow updates
+func (t *Thing) SubscribeForThingShadowChanges() (chan Shadow, chan ShadowError, error) {
 	shadowChan := make(chan Shadow)
+	shadowErrChan := make(chan ShadowError)
 
-	token := t.client.Subscribe(
+	if token := t.client.Subscribe(
 		fmt.Sprintf("$aws/things/%s/shadow/update/accepted", t.thingName),
 		0,
 		func(client mqtt.Client, msg mqtt.Message) {
 			shadowChan <- msg.Payload()
 		},
+	); token.Wait() && token.Error() != nil {
+		return nil, nil, token.Error()
+	}
+
+	if token := t.client.Subscribe(
+		fmt.Sprintf("$aws/things/%s/shadow/update/rejected", t.thingName),
+		0,
+		func(client mqtt.Client, msg mqtt.Message) {
+			shadowErrChan <- msg.Payload()
+		},
+	); token.Wait() && token.Error() != nil {
+		return nil, nil, token.Error()
+	}
+
+	return shadowChan, shadowErrChan, nil
+}
+
+// UpdateThingShadowDocument publishes an async message with new thing shadow document
+func (t *Thing) UpdateThingShadowDocument(payload Shadow) error {
+	token := t.client.Publish(fmt.Sprintf("$aws/things/%s/shadow/update/documents", t.thingName), 0, false, []byte(payload))
+	token.Wait()
+	return token.Error()
+}
+
+// DeleteThingShadow publishes a message to remove the device's shadow and waits for the result. In case shadow delete was
+// rejected the method will return error
+func (t *Thing) DeleteThingShadow() error {
+	shadowChan := make(chan Shadow)
+	errChan := make(chan error)
+
+	defer t.unsubscribe(
+		fmt.Sprintf("$aws/things/%s/shadow/delete/accepted", t.thingName),
+		fmt.Sprintf("$aws/things/%s/shadow/delete/rejected", t.thingName),
+	)
+
+	if token := t.client.Subscribe(
+		fmt.Sprintf("$aws/things/%s/shadow/delete/accepted", t.thingName),
+		0,
+		func(client mqtt.Client, msg mqtt.Message) {
+			shadowChan <- msg.Payload()
+		},
+	); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	if token := t.client.Subscribe(
+		fmt.Sprintf("$aws/things/%s/shadow/delete/rejected", t.thingName),
+		0,
+		func(client mqtt.Client, msg mqtt.Message) {
+			errChan <- errors.New(string(msg.Payload()))
+		},
+	); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	if token := t.client.Publish(
+		fmt.Sprintf("$aws/things/%s/shadow/delete", t.thingName),
+		0,
+		false,
+		[]byte("{}"),
+	); token.Wait() && token.Error() != nil {
+		return token.Error()
+	}
+
+	for {
+		select {
+		case _, ok := <-shadowChan:
+			if !ok {
+				return errors.New("failed to read from shadow channel")
+			}
+			return nil
+		case err, ok := <-errChan:
+			if !ok {
+				return errors.New("failed to read from error channel")
+			}
+			return err
+		}
+	}
+}
+
+// PublishToCustomTopic publishes an async message to the custom topic.
+// The specified topic argument will be prepended by a prefix "$aws/things/<thing_name>"
+func (t *Thing) PublishToCustomTopic(payload Shadow, topic string) error {
+	token := t.client.Publish(
+		path.Join("$aws/things", t.thingName, topic),
+		0,
+		false,
+		[]byte(payload),
 	)
 	token.Wait()
+	return token.Error()
+}
 
-	return shadowChan, token.Error()
+// SubscribeForCustomTopic subscribes for the custom topic and returns the channel with the topic messages.
+// The specified topic argument will be prepended by a prefix "$aws/things/<thing_name>"
+func (t *Thing) SubscribeForCustomTopic(topic string) (chan Shadow, error) {
+	shadowChan := make(chan Shadow)
+
+	if token := t.client.Subscribe(
+		path.Join("$aws/things", t.thingName, topic),
+		0,
+		func(client mqtt.Client, msg mqtt.Message) {
+			shadowChan <- msg.Payload()
+		},
+	); token.Wait() && token.Error() != nil {
+		return nil, token.Error()
+	}
+
+	return shadowChan, nil
+}
+
+// UnsubscribeFromCustomTopic terminates the subscription to the custom topic.
+// The specified topic argument will be prepended by a prefix "$aws/things/<thing_name>"
+func (t Thing) UnsubscribeFromCustomTopic(topic string) error {
+	return t.unsubscribe(path.Join("$aws/things", t.thingName, topic))
+}
+
+// unsubscribe terminates the MQTT subscription for the provided tokens
+func (t Thing) unsubscribe(topics ...string) error {
+	token := t.client.Unsubscribe(topics...)
+	token.Wait()
+	return token.Error()
 }
--- device/thing_test.go
+++ device/thing_test.go
@@ -8,8 +8,9 @@ import (
 	"time"
 )
 
-var thingName = ThingName("test")
+var thingName = "test"
 var endpoint = "a1xumedgml9iv9-ats.iot.us-east-1.amazonaws.com"
+
 //var region = Region("us-east-1")
 var keyPair = KeyPair{
 	CertificatePath:   "./certificates/cert.pem",
@@ -29,14 +30,17 @@ func TestNewThing(t *testing.T) {
 	thing, err := NewThing(keyPair, endpoint, thingName)
 	assert.NoError(t, err, "thing instance created without error")
 	assert.NotNil(t, thing, "thing instance is not nil")
+
+	defer thing.Disconnect()
 }
 
 func TestThingShadow(t *testing.T) {
 	thing, err := NewThing(keyPair, endpoint, thingName)
 	assert.NoError(t, err, "thing instance created without error")
 	assert.NotNil(t, thing, "thing instance is not nil")
+	defer thing.Disconnect()
 
-	thingShadowChann, err := thing.SubscribeForThingShadowChanges()
+	thingShadowChan, _, err := thing.SubscribeForThingShadowChanges()
 	assert.NoError(t, err, "received thing shadow subscription channel without error")
 
 	data := time.Now().Unix()
@@ -46,7 +50,7 @@ func TestThingShadow(t *testing.T) {
 	err = thing.UpdateThingShadow(Shadow(shadow))
 	assert.NoError(t, err, "thing shadow updated without error")
 
-	updatedShadow, ok := <-thingShadowChann
+	updatedShadow, ok := <-thingShadowChan
 	assert.True(t, ok, "the reading updated shadow channel was successful")
 
 	unmarshaledUpdatedShadow := &shadowStruct{}
@@ -59,10 +63,79 @@ func TestThingShadow(t *testing.T) {
 	gottenShadow, err := thing.GetThingShadow()
 	assert.NoError(t, err, "retrieved thing shadow without error")
 
-	unmarshalledGottenShadow := &shadowStruct{}
+	unmarshaledGottenShadow := &shadowStruct{}
 
-	err = json.Unmarshal(gottenShadow, unmarshalledGottenShadow)
+	err = json.Unmarshal(gottenShadow, unmarshaledGottenShadow)
 	assert.NoError(t, err, "retrieved thing shadow unmarshaling  without error")
 
-	assert.Equal(t, data, unmarshalledGottenShadow.State.Reported.Value, "retrieved thing shadow has consistent data")
+	assert.Equal(t, data, unmarshaledGottenShadow.State.Reported.Value, "retrieved thing shadow has consistent data")
+}
+
+func TestThing_UpdateThingShadowShouldFail(t *testing.T) {
+	thing, err := NewThing(keyPair, endpoint, thingName)
+	assert.NoError(t, err, "thing instance created without error")
+	assert.NotNil(t, thing, "thing instance is not nil")
+	defer thing.Disconnect()
+
+	_, thingShadowErrChan, err := thing.SubscribeForThingShadowChanges()
+	assert.NoError(t, err, "received thing shadow subscription channel without error")
+
+	err = thing.UpdateThingShadow(Shadow("invalid JSON"))
+	assert.NoError(t, err, "thing shadow updated without error")
+
+	_, ok := <-thingShadowErrChan
+	assert.True(t, ok, "the update shadow error has been handled successfully")
+}
+
+func TestThing_UpdateThingShadowDocument(t *testing.T) {
+	thing, err := NewThing(keyPair, endpoint, thingName)
+	assert.NoError(t, err, "thing instance created without error")
+	assert.NotNil(t, thing, "thing instance is not nil")
+	defer thing.Disconnect()
+
+	shadowChan, err := thing.SubscribeForCustomTopic("shadow/update/documents")
+	assert.NoError(t, err, "received thing shadow subscription channel without error")
+
+	shadowDocument := fmt.Sprintf(`{"state": {"reported": {"value": %d}}}`, time.Now().UTC().Unix())
+
+	err = thing.UpdateThingShadowDocument(Shadow(shadowDocument))
+	assert.NoError(t, err, "thing shadow document updated without error")
+
+	remoteShadow, ok := <- shadowChan
+	assert.True(t, ok, "the update shadow document has been handled successfully")
+
+	assert.Equal(t, Shadow(shadowDocument), remoteShadow)
+}
+
+func TestThing_DeleteThingShadow(t *testing.T) {
+	thing, err := NewThing(keyPair, endpoint, thingName)
+	assert.NoError(t, err, "thing instance created without error")
+	assert.NotNil(t, thing, "thing instance is not nil")
+	defer thing.Disconnect()
+
+	err = thing.DeleteThingShadow()
+	assert.NoError(t, err, "thing shadow deleted without error")
+}
+
+func TestThing_CustomTopic(t *testing.T) {
+	thing, err := NewThing(keyPair, endpoint, thingName)
+	assert.NoError(t, err, "thing instance created without error")
+	assert.NotNil(t, thing, "thing instance is not nil")
+	defer thing.Disconnect()
+
+	customTopic := "fancy"
+
+	shadowChan, err := thing.SubscribeForCustomTopic(customTopic)
+	assert.NoError(t, err, "received thing shadow custom topic subscription channel without error")
+
+	shadowPayload := Shadow(`{"state":{"reported":{"yo":true}}}`)
+
+
+	err = thing.PublishToCustomTopic(shadowPayload, customTopic)
+	assert.NoError(t, err, "thing shadow published to custom topic updated without error")
+
+	remoteShadow, ok := <- shadowChan
+	assert.True(t, ok, "the shadow in custom topic has been handled successfully")
+
+	assert.Equal(t, shadowPayload, remoteShadow)
 }
